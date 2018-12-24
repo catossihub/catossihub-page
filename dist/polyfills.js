b00ab76672ed5b118828c576076a7eddb12d3781
augbond <augusto.bondanca@accenture.com>
2018-12-24 00:01:03 -0200

    new version with carousel

--- dist/polyfills.js
+++ dist/polyfills.js
@@ -5473,7 +5473,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /home/augusto/Documents/projetos/catossihub-page/src/polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! /home/augusto-bondanca/Documents/projetos/catossihub/catossihub.github.io/src/polyfills.ts */"./src/polyfills.ts");
 
 
 /***/ })

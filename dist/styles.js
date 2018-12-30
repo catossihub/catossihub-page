96e547c42a56c67abd2db8922d8d421ec6827481
AugustoBondanca <augustobondancaa@gmail.com>
2018-12-30 14:29:53 -0200

    disy

--- dist/styles.js
+++ dist/styles.js
@@ -539,7 +539,7 @@ if(false) {}
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /home/augusto-bondanca/Documents/projetos/catossihub/catossihub.github.io/src/styles.css */"./src/styles.css");
+module.exports = __webpack_require__(/*! /home/augusto/Documentos/catossihub/catossihub.github.io/src/styles.css */"./src/styles.css");
 
 
 /***/ })
